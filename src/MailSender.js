19a4cd2873237b71e7c1d013ce07ce69514ef6c0
Hidayat Chandra <hidayatchandra08@gmail.com>
2023-10-05 17:19:04 +0800

    add: mail sender

---
+++ src/MailSender.js
@@ -0,0 +1,33 @@
+import nodemailer from 'nodemailer';
+
+class MailSender {
+  constructor() {
+    this._transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD,
+      },
+    });
+  }
+
+  sendEmail(targetEmail, content) {
+    const message = {
+      from: 'Notes App',
+      to: targetEmail,
+      subject: 'Export Notes',
+      text: 'Result from export notes',
+      attachments: [
+        {
+          filename: 'notes.json',
+          content,
+        },
+      ],
+    };
+
+    return this._transporter.sendMail(message);
+  }
+}
+
+export default MailSender;
